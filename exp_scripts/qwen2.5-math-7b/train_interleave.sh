a04513fdc2eefadbacf98d062d5419dff767872c
root <root@TENCENT64.site>
2025-05-21 15:45:45 +0800

    s

--- exp_scripts/qwen2.5-math-7b/train_interleave.sh
+++ exp_scripts/qwen2.5-math-7b/train_interleave.sh
@@ -20,8 +20,9 @@ python -m verl.src_interleave_sft.main_mix_ppo \
     +actor_rollout_ref.actor.sft.sft_data_size=64 \
     +actor_rollout_ref.actor.sft.sft_mini_batch_size=64 \
     +actor_rollout_ref.actor.sft.sft_micro_batch_size=64 \
-    +actor_rollout_ref.actor.sft.entropy_coeff=0.0 \
+    +actor_rollout_ref.actor.sft.entropy_coeff=0.001 \
     +actor_rollout_ref.actor.optim.sft.lr=1e-5 \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
     algorithm.adv_estimator=grpo \
     data.train_files=$DATA_DIR/openr1.parquet \
     data.val_files=$DATA_DIR/valid.parquet \
@@ -30,8 +31,7 @@ python -m verl.src_interleave_sft.main_mix_ppo \
     data.max_prompt_length=1024 \
     data.max_response_length=8192 \
     actor_rollout_ref.model.path=$MODEL_PATH \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.actor.grad_clip=1.0 \
+    actor_rollout_ref.actor.grad_clip=0.7 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.ppo_micro_batch_size=64 \
@@ -44,6 +44,7 @@ python -m verl.src_interleave_sft.main_mix_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.grad_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    +actor_rollout_ref.actor.fsdp_config.sft_optimizer_offload=False \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.temperature=1.0 \
